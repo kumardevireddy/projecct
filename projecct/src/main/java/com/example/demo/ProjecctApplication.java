3a6aa63273db99deb8398eaba57fadddf536c736
kumardevireddy <69845251+kumardevireddy@users.noreply.github.com>
2020-08-20 11:50:58 +0530

    first commit

---
+++ projecct/src/main/java/com/example/demo/ProjecctApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecctApplication.class, args);
+	}
+
+}
